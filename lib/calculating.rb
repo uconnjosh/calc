f6a7795a82172ff1f0ed3d9f5fe0a329bd406d7f
Josh Paul <joshua.paul@uconn.edu>
2014-03-06 10:38:54 -0800

    calculator thingy done!e

--- lib/calculating.rb
+++ lib/calculating.rb
@@ -1,19 +1,29 @@
+def equation_separator(string)
+  questions = string.split("?")
+  questions.each { |question| puts "The answer to '#{question}' is: #{calc(question)}" }
+end
+
+
 def calc(string)
   eng_eq = string.split(" ")
-  token_dict = {"plus" => "+"}
+  token_dict = {"plus" => "+","minus" => "-", "times" => "*", "divided" => "/",
+                "to" => "**"}
 
   math_eq = []
   eng_eq.each do |token|
-    if token.downcase == "what" || token.downcase == "is"
+    if token.downcase == "what" || token.downcase == "is" ||
+       token.downcase == "by"  || token.downcase == "what's" ||
+       token.downcase == "the" || token.downcase == "power" ||
+       token.downcase == "of"
 
     elsif token_dict.has_key?(token)
       math_eq << token_dict[token]
     else
-      math_eq << token
+      math_eq << token.to_f
     end
   end
 
-  eval(math_eq.join(""))
+  eval(math_eq.join(" "))
 end
 
-print calc("What is 5 plus 4")
+equation_separator("What is 5 plus 3?What is 5 times 3?")
--- spec/calculating_spec.rb
+++ spec/calculating_spec.rb
@@ -6,13 +6,17 @@ describe('calc') do
     calc("What is 5 plus 4").should(eq(9))
   end
   it('Takes a sentence and find correct val for sub, mult, div') do
-    calc("What is 5 / 4").should(eq())   
-    calc("What is 6 * 9").should(eq())
-    calc("What is 6 - 9").should(eq())
+    calc("What is 5 divided by 4").should(eq(1.25))   
+    calc("What is 6 times 9").should(eq(54))
+    calc("What is 6 minus 9").should(eq(-3))
   end
   it('Takes a sentence regardless of syntax (within reason) and returns correct value') do
-    calc("wHat IS 5.3-9").should(eq())
-    calc("what is 0 + 5?").should(eq())
-    calc("what's (9+9)").should(eq())
+    calc("wHat IS 5.3 minus 9").should(eq(-3.70))
+    calc("what is 0 plus 5?").should(eq(5))
+    calc("what's 9 plus 9").should(eq(18))
+  end
+  it('Does powers of when called for') do
+    calc("What is 2 to the 3rd power").should(eq(8))
   end
 end
+
